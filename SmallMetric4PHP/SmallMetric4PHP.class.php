5bf0feb7e7ad2c37bc074090a083742201d5c755
Sebastian Krüger <krueger@secra.de>
2013-07-01 14:41:04 +0200

    Start Class and Example Page
    Not much on the first step

---
+++ SmallMetric4PHP/SmallMetric4PHP.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * SmallMetric4PHP - Performance-Timer
+ *
+ * Copyright (c) 2013 Sebastian Krüger
+ *
+ * @package SmallMetric4PHP
+ * @file SmallMetric4PHP/SmallMetric4PHP.class.php
+ */
+
+namespace SmallMetric4PHP;
+
+class SmallMetric4PHP {
+
+}
---
+++ example.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * SmallMetric4PHP - Performance-Timer
+ *
+ * Copyright (c) 2013 Sebastian Krüger
+ *
+ * @package SmallMetric4PHP
+ * @file example.php
+ */
+
+// Include the Class
+require_once 'SmallMetric4PHP/SmallMetric4PHP.class.php';
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<title>Example page to show the awesomeness of the PHP Small Metric class</title>
+</head>
+<body>
+
+<p>Start up Class an run the script!</p>
+<?php
+
+
+?>
+</body>
+</html>
